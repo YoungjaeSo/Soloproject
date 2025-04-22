48c29dc835f913458ce98ee811cd30b6dcccd567
Youngjae so <terranbang@gmail.com>
2025-04-22 17:01:24 +0900

    add: Login페이지 틀, MenuNav틀, router, db.json 임시 추가

---
+++ src/views/MenuNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <button class="">Home</button>
+    <button class="">게시판</button>
+    <button class="">외부 사이트 링크</button>
+    <button class="">회원정보 수정</button>
+  </div>
+</template>
+<script></script>
+<style></style>
